2388b64b34f42e3593fcda13f3981543be6092b5
radarsnd <radarsnd@gmail.com>
2025-03-15 21:39:06 +0700

    Fixed ESLint error - removed unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Contact from "@/components/Contact";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+
 
 
 
